1fdc3515f30e74cc750ec830b551358966f3367a
Sam Maier <sam@sammaier.ca>
2017-08-09 17:51:55 -0400

    E2E working

--- btooth.py
+++ btooth.py
@@ -1,4 +1,5 @@
 import bluetooth
+import struct
 from pprint import pprint
 
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
@@ -6,14 +7,16 @@ print("Found %d devices" % len(nearby_devices))
 
 num = 0
 for addr, name in nearby_devices:
-    print("#%d  %s - %s" % (num, addr, name))
-    num += 1
+    try:
+        print("#%d  %s - %s" % (num, addr, name))
+        num += 1
+    except:
+        pass
 
 dev_num = int(input("Select which device: "))
 
 device = nearby_devices[dev_num]
 services = bluetooth.find_service(address=device[0])
-
 
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 service = None
@@ -28,10 +31,48 @@ else:
     exit()
 
 sock.send("".join(map(chr,[8])))
-sock.send("temp.txt")
+sock.send("boii.txt")
 sock.send("".join(map(chr,[9])))
-sock.send("TESTING!_")
+sock.send("YEAAAAA!_")
 
 sock.close()
 
+
+#RECV
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("Found %d devices" % len(nearby_devices))
+
+num = 0
+for addr, name in nearby_devices:
+    print("#%d  %s - %s" % (num, addr, name))
+    num += 1
+
+dev_num = int(input("Select which device: "))
+
+device = nearby_devices[dev_num]
+services = bluetooth.find_service(address=device[0])
+
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+service = None
+for s in services:
+    if s['service-classes'][0] == "B10E7007-CCD4-BBD7-1AAA-5EC0000000FF":
+        service = s
+
+if service:
+    sock.connect((device[0], service['port']))
+else:
+    pprint(services)
+    exit()
+
 pprint(service)
+
+
+size = struct.unpack(">L", sock.recv(4))[0]
+file_name = sock.recv(size)
+size = struct.unpack(">L", sock.recv(4))[0]
+pad_contents = sock.recv(size)
+
+print file_name
+print pad_contents
+
+sock.close()
